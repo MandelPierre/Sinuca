86b4693f26db791aa009089ac5902038c030732e
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-09-30 21:56:55 -0300

    Continuação do projeto

--- demo/src/main/java/sinuca/example/Main.java
+++ demo/src/main/java/sinuca/example/Main.java
@@ -1,23 +1,39 @@
 package sinuca.example;
 
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Qua valor da ficha (Cada paritda)? ");
+        double valorFicha = sc.nextDouble();
+        sc.nextLine();
+
         System.out.println("Quantos jogadores pariciparam? ");
         int qtdJogadores = sc.nextInt();
         sc.nextLine();
 
+        List<Jogador> jogadores = new ArrayList<>();
+
+
         for (int i = 1; i <= qtdJogadores; i++) {
             System.out.println("Digite o nome dos jogadores " + i + " : ");
             String nome = sc.nextLine();
-
-
-            System.out.println("Jogador cadastro: " + nome);
+            jogadores.add(new Jogador(nome));
         }
 
-        sc.close();
+        System.out.println("Quantas partidas foram jogadas? ");
+        int qtdPartidas = sc.nextInt();
+        sc.nextLine();
+        
+        List<Partida> partidas = new ArrayList<>();
+
+        //Cada partida perguntar quem jogou
+        for (int p = 1; p <= qtdPartidas; p++) {
+            System.out.println("-----Partida "+ p + "-----");
+        }
     }
 }
