aeb250d62f57dfcd5e7e53fbc6763713478b5565
PierreMandel <202198719+MandelPierre@users.noreply.github.com>
2025-10-06 21:43:04 -0300

    Adicionando a Web

---
+++ demo/src/main/java/sinuca/example/SinucaApplication.java
@@ -0,0 +1,11 @@
+package sinuca.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinucaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SinucaApplication.class, args);
+    }
+}
